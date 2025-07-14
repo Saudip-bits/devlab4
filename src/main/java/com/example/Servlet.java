ba257ce03870b713d93c68666fa5b887f4a4bd0b
Saudip-bits <2021wb86349@wilp.bits-pilani.ac.in>
2025-07-14 20:33:31 +0530

    Update Servlet.java

--- src/main/java/com/example/Servlet.java
+++ src/main/java/com/example/Servlet.java
@@ -11,10 +11,10 @@ public class Servlet extends HttpServlet {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         out.println("<html><head><title>Devops Lab 4</title></head><body>");
-        out.println("<h1>Welcome to Tomcat</h1>");
+        out.println("<h1>Hello World</h1>");
         out.println("<ul>");
-        out.println("<li>Jereme</li>");
-        out.println("<li>2021WB86864</li>");
+        out.println("<li>Saudip Santra</li>");
+        out.println("<li>2021wb86349</li>");
         out.println("</ul>");
         out.println("</body></html>");
     }
