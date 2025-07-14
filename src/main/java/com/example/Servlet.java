4ca3889e9a10cac3c3420b92f6764e059237358b
Saudip-bits <2021wb86349@wilp.bits-pilani.ac.in>
2025-07-14 20:31:54 +0530

    Add files via upload

---
+++ src/main/java/com/example/Servlet.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+public class Servlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><head><title>Devops Lab 4</title></head><body>");
+        out.println("<h1>Welcome to Tomcat</h1>");
+        out.println("<ul>");
+        out.println("<li>Jereme</li>");
+        out.println("<li>2021WB86864</li>");
+        out.println("</ul>");
+        out.println("</body></html>");
+    }
+}
+
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,12 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Lab4</title>
+</head>
+<body>
+<h1>Welcome to DevOps Lab4 - Maven & Git</h1>
+<p><a href="info">Click here to view info</a></p>
+<h1> By Jereme - 2021WB86864 </h1>
+
+</body>
+</html>
